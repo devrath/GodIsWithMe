8b7db7709774390d0a5f1ce1a925cf102e2172d1
Devrath <devrath.dev595@gmail.com>
2024-11-02 08:28:16 +0530

    dependencies added

--- Code/core/presentation/designsystem/build.gradle.kts
+++ Code/core/presentation/designsystem/build.gradle.kts
@@ -36,7 +36,18 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.material)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.material3.size)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
