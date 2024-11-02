a5500d774ec2d413cb744ed6247446e5b22a0764
Devrath <devrath.dev595@gmail.com>
2024-11-02 09:49:58 +0530

    initial setup

---
+++ Code/app/src/main/java/com/istudio/godiswithme/ui/designsystem/components/util/DropDownItem.kt
@@ -0,0 +1,8 @@
+package com.istudio.godiswithme.ui.designsystem.components.util
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class DropDownItem(
+    val icon: ImageVector,
+    val title: String
+)
